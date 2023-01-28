f81a69c430b8d47a69ec8c086e1c46036e02dabb
Marc Girbau <marc.girbau94@gmail.com>
2023-01-28 15:27:51 +0100

    Fix CardComponent innerHTML

--- src/ts/components/CardComponent/CardComponent.ts
+++ src/ts/components/CardComponent/CardComponent.ts
@@ -16,15 +16,15 @@ export class CardComponent extends Component implements CardComponentStructure {
 
     this.element.innerHTML = `
     <a>
-     <div class="pokemon-card__info">
-       <span class="pokemon-card__name">${this.pokemon.pokemonName}</span>
-       <span class="pokemon-card__id">#${this.pokemon.id}</span>
-       <span class="pokemon-card__type">${this.pokemon.types.name}</span>
-     </div>
-     <div class="pokemon-card__image-container">
-       <img class="pokemon-card__image" src="${this.pokemon.sprites.dream_world.front_default}">
-     </div>
-  </a>
+      <div class="pokemon-card__info">
+        <span class="pokemon-card__name">${this.pokemon.pokemonName}</span>
+        <span class="pokemon-card__id">#${this.pokemon.id}</span>
+        <span class="pokemon-card__type">${this.pokemon.types.name}</span>
+      </div>
+      <div class="pokemon-card__image-container">
+        <img class="pokemon-card__image" src="${this.pokemon.sprites.dream_world.front_default}">
+      </div>
+    </a>
     `;
   }
 }
