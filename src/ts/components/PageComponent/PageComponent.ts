91c1f09b34081e3e508d99d3f43f96b55de6e6aa
Marc Girbau <marc.girbau94@gmail.com>
2023-01-28 23:48:52 +0100

    Add functionality to pagination buttons

--- src/ts/components/PageComponent/PageComponent.ts
+++ src/ts/components/PageComponent/PageComponent.ts
@@ -1,3 +1,4 @@
+import { startApp } from "../../index.js";
 import type { Pokemon } from "../../types.js";
 import { CardListComponent } from "../CardListComponent/CardListComponent.js";
 import { Component } from "../Component/Component.js";
@@ -6,8 +7,8 @@ export class PageComponent extends Component {
   constructor(
     public title: string,
     public pokemons: Pokemon[],
-    private readonly firstPosiiton: number,
-    private readonly lastPosition: number
+    private firstPositon = 0,
+    private lastPosition = 10
   ) {
     super("div", document.body, "app");
 
@@ -26,7 +27,10 @@ export class PageComponent extends Component {
       <input type="search" placeholder="🔍  What Pokemon are you looking for?" class="main-header__searchbar">
       <div class="main-header__links link">
         <a class="link__favourite">My Pokemons</a>
-       
+        <div class="link__buttons buttons">
+          <button class="buttons__back"></button>
+          <button class="buttons__forward"></button>
+        </div>
       </div>
     </header>
     <main class="container"></main>
@@ -35,8 +39,36 @@ export class PageComponent extends Component {
     const listContainer = this.element.querySelector(".container");
     const listOfPokemons = new CardListComponent(
       listContainer,
-      this.pokemons.slice(this.firstPosiiton, this.lastPosition)
+      this.pokemons.slice(this.firstPositon, this.lastPosition)
     );
     listOfPokemons.render();
+
+    const forwardButton = this.element.querySelector(".buttons__forward");
+    forwardButton.addEventListener("click", () => {
+      this.firstPositon += 10;
+      this.lastPosition += 10;
+      listContainer.innerHTML = "";
+
+      const listOfPokemons = new CardListComponent(
+        listContainer,
+        this.pokemons.slice(this.firstPositon, this.lastPosition)
+      );
+      listOfPokemons.render();
+    });
+
+    const backButton = this.element.querySelector(".buttons__back");
+    backButton.addEventListener("click", () => {
+      if (this.firstPositon !== 0) {
+        this.firstPositon -= 10;
+        this.lastPosition -= 10;
+        listContainer.innerHTML = "";
+
+        const listOfPokemons = new CardListComponent(
+          listContainer,
+          this.pokemons.slice(this.firstPositon, this.lastPosition)
+        );
+        listOfPokemons.render();
+      }
+    });
   }
 }
